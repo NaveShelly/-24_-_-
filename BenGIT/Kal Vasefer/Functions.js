a157490b921504844df93b36bba1726af4a63755
NaveShelly <58579875+NaveShelly@users.noreply.github.com>
2019-12-09 19:23:56 +0200

    Add files via upload

--- BenGIT/Kal Vasefer/Functions.js
+++ BenGIT/Kal Vasefer/Functions.js
@@ -35,6 +35,30 @@ function savedetails(username1,Fname,Lname,pw,maildetail)
 	//localStorage.setItem('pw', pw);
 	//localStorage.setItem('email', maildetail)
 }
+function unitestpages()
+{
+	if(window.location("register.html")==null)
+		return alert("register page error")+false;
+	if(window.location("connection.html")==null)
+		return alert("connection page error")+false;
+	if(window.location("userguide.html")==null)
+		return alert("userguide page error")+false;
+	if(window.location("FrontPageExample.html")==null)
+		return alert("FrontPageExample page error")+false;
+}
+function unitestuserdetails(username1,Fname,Lname,pw,maildetail)
+{
+	if (username1 == null)
+		return alert("username wrong") + false;
+	if (Fname == null)
+		return alert("First Name wrong") + false;
+	if (Lname == null)
+		return alert("Last Name wrong") + false;
+	if (pw == null)
+		return alert("password Wrong") + false;
+	if (maildetail == null)
+		return alert("mail wrong") + false;
+}
 function LionExam()
 {
 	window.location="LionExam.html"

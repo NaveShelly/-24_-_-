b312b07e4281b8757024f05695bdb2e89613a879
Benjemin12 <58631805+Benjemin12@users.noreply.github.com>
2019-12-22 21:35:35 +0200

    Add files via upload

--- Person.h
+++ Person.h
@@ -1,27 +1,53 @@
 #include <iostream>
+
 #include <string>
+
 #ifndef PERSON_H
+
 #define PERSON_H
+
 using namespace std;
 
-class Person {
+
+
+class  Person{
+
+
 
 private:
-	string name;
-	long id;
-	int age;
+
+	
 
 public:
+	string name;
+
+	long id;
+
+	int age;
+
+
 	Person();
+
 	Person(string, long, int);
+
 	Person(const Person&);
+
 	~Person();
+
 	virtual void SetPerson();
+
 	inline string getName() { return name; }
+
 	inline long getID() { return id; }
+
 	inline int getAge() { return age; }
 
 
 
+
+
+
+
 };
+
 #endif

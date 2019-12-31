27a5c02a39f178c48b45a3b62c9758d0878f41ae
Linoy <linoyas@ac.sce.ac.il>
2019-12-31 23:50:25 +0200

    The page of register parent

---
+++ save3.php
@@ -0,0 +1,18 @@
+<?php
+	include 'database.php';
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$password=$_POST['password'];
+		$sql = "INSERT INTO `parent`( `name`, `email`, `password`) 
+	    VALUES ('$name','$email','$password')";
+	    if (mysqli_query($conn, $sql)) {
+		        echo json_encode(array("statusCode"=>200));
+	        } 
+	        else {
+		        echo json_encode(array("statusCode"=>201));
+	        }
+
+		mysqli_close($conn);
+
+?>
+ 

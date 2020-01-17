07114eed44d1e9f07093344943dff78bd49e8620
Benjemin12 <58631805+Benjemin12@users.noreply.github.com>
2020-01-17 17:38:00 +0200

    Lady Pinky Functions Like Check Answers

---
+++ BenGIT/Kal Vasefer/LadyPinkyFunctions.js
@@ -0,0 +1,37 @@
+function LadyPinkExam()
+{
+    window.location="LadyPinkyExam.php"
+}
+function CheckAnswer()
+{var Correct=0
+    var Question1=document.Quize.Question1.value
+    var Question2=document.Quize.Question2.value
+    var Question3=document.Quize.Question3.value
+    var Question4=document.Quize.Question4.value
+    var Question5=document.Quize.Question5.value
+    if(Question1=="Right")
+    {
+        Correct++
+    }
+    if(Question2=="Right")
+    {
+        Correct++
+    }
+    if(Question3=="Right")
+    {
+        Correct++
+    }
+    if(Question4=="Right")
+    {
+        Correct++
+    }
+    if(Question5=="Right")
+    {
+        Correct++
+    }
+    alert("Your Grade Is "+ Correct*20)
+    document.getElementById("ShowResult").innerHTML="Your Grade Is "+ Correct*20
+
+
+
+}

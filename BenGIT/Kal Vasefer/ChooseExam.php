928f6c0c2968e435d8624ddb78f280b6fb75bba8
NaveShelly <58579875+NaveShelly@users.noreply.github.com>
2020-01-17 02:52:26 +0200

    Add files via upload
    file for users choosing what grade to get

---
+++ BenGIT/Kal Vasefer/ChooseExam.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <h1 style="font-size:25px;text-align:center;color:blue;">קל וספר- אתר מלווה ספר קריאה</h1>
+    <title> Kal Vasefer Example </title>
+    <meta charset="UTF-8">
+    <title>Insert title here</title>
+</head>
+<body>
+<div style="position: absolute; left:100px; top:62px;">
+<button id="button1" onclick="Frontpage()">דף עידכונים</button>
+</div>
+<div style="position: absolute; left:400px; top:62px;">
+<button id="button3" onclick="howtouse()">מדריך שימוש באתר</button>
+</div>
+<div style="position: absolute; left:1100px; top:62px;">
+<button id="button6" onclick="frontpage()">ראשי</button>
+</div>
+<div style="position: absolute; left:1000px; top:62px;">
+<button id="button7" onclick="connectionpage()">צור קשר</button>
+</div>
+<div style="position: absolute; left:900px; top:62px;">
+<button id="button8" onclick="parent_login()">כניסת הורה</button>
+</div>
+<div style="position: absolute; left:800px; top:62px;">
+<button id="button9" onclick=" regpageparent()">הרשמה הורה</button>
+</div>
+<div style="position: absolute; left:600px; top:62px;">
+<button id="button10" onclick=" GetGrades()">משוב ציונים</button>
+<div style="position: absolute; left:150px; top:300px;">
+<button id="button11" onclick="CheckLionExam()" style="width:150px;height:100px;">ציון מבחן 1 האריה שאהב תות</button>
+<button id="button12" onclick="CheckBaloonExam()" style="width:150px;height:100px;">ציון מבין 2 מעשה בחמישה בלונים</button>
+<button id="button13" onclick="CheckPinkyExam()" style="width:150px;height:100px;">ציון מבחן 3 גברת זרת</button>>
+</div>
+<script src= "Functions.js"></script>
+</body>
+</html>

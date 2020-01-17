245d139972b181689d26235e0a0d3efd003990bf
Benjemin12 <58631805+Benjemin12@users.noreply.github.com>
2020-01-17 17:33:32 +0200

    JavaScript Functions For Checking The Test Answers

---
+++ BenGIT/Kal Vasefer/Check.js
@@ -0,0 +1,35 @@
+function BaloonExam()
+{
+    window.location="FiveBaloonsExam.php"
+}
+function CheckAnswer()
+{var Correct=0
+    var Question1=document.Quize.Question1.value
+    var Question2=document.Quize.Question2.value
+    var Question3=document.Quize.Question3.value
+    var Question4=document.Quize.Question4.value
+    var Question5=document.Quize.Question5.value
+    if(Question1=="Right")
+    {
+        Correct++
+    }
+    if(Question2=="Right")
+    {
+        Correct++
+    }
+    if(Question3=="Right")
+    {
+        Correct++
+    }
+    if(Question4=="Right")
+    {
+        Correct++
+    }
+    if(Question5=="Right")
+    {
+        Correct++
+    }
+    document.getElementById('first_name').value=Correct*20
+    //return Correct*20
+
+}

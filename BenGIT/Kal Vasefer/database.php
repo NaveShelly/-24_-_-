5fbadade1813d0fcebe15b2362332a87724ae370
NaveShelly <58579875+NaveShelly@users.noreply.github.com>
2020-01-17 14:43:08 +0200

    Delete database.php

--- BenGIT/Kal Vasefer/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "id12081626_kalvasefer";
-	$password = "Kalvasefer";
-	$db="id12081626_kalvasefer";
-	$conn = mysqli_connect($servername, $username, $password,$db);
-?>
-

59a3eb0087aa49a0fae8e7810edcb9d41acf4941
Benjemin12 <58631805+Benjemin12@users.noreply.github.com>
2020-01-17 17:36:26 +0200

    General Functions For Our Project Like Switching Pages

--- BenGIT/Kal Vasefer/Functions.js
+++ BenGIT/Kal Vasefer/Functions.js
@@ -1,70 +1,184 @@
 /**
  * 
  */
+function gradebutton()
+{
+window.location="takegrade.html";
+}
+function getgrade1()
+{
+    window.location="https://kalvasefer.000webhostapp.com/checktable.php";
+}
+function getgrade2()
+{
+window.location="https://kalvasefer.000webhostapp.com/CheckTable2.php";
+}
+function getgrade3()
+{
+    window.location="https://kalvasefer.000webhostapp.com/CheckTable3.php";
+}
+function regpageparent(){
+    window.location="parentsignup.php";
+}
+function funcLogin()
+{
+    window.location="login.html";
+}
+function CheckLionExam()
+{
+    window.location.href="https://kalvasefer.000webhostapp.com/CheckTable1.php";
+}
+function CheckBaloonExam()
+{
+    window.location="CheckTable2.php";
+}   
+function CheckPinkyExam()
+{
+    window.location="CheckTable3.php";
+}
 function frontpage()
 {
 	window.location= "FrontPageExample.html";
+}
+function adminpage()
+{
+    window.location="adminloginpage.html";
 }
 function secondpage()
 {
 	window.location="odot.html";
 }
+function logout()
+{
+    window.location= "FrontPageExample.html";
+}
+function userguideusers()
+{
+    window.location="https://kalvasefer.000webhostapp.com/userguideusers.html";
+}
+function adminlogin(pw) 
+{
+    var input = document.getElementById('pw').value;
+    if(input=='admin12345')
+    {
+        location.href = "https://il.000webhost.com/members/website/kalvasefer/database";
+        alert("Moving to Database");
+    }
+    else
+    {
+        alert("Worng Password");
+    }
+}
+function loginpages()
+{
+     location.href = "https://kalvasefer.000webhostapp.com/loginspages.html"
+}
+function connectionpageusers()
+{
+    window.location="connectionuser.html";
+}
 function howtouse()
 {
-	window.location="userguide.html"
+	window.location="userguide.html";
+}
+function userpages()
+{
+    location.href = "userHomePage.html";
 }
 function regpage()
 {
-	window.location="register.html";
+	window.location="https://kalvasefer.000webhostapp.com/signup.php";
 }
 function connectionpage()
 {
 	window.location="connection.html";
 }
-function savedetails(username1,Fname,Lname,pw,maildetail)
+function connectionpageparent()
 {
-	var input1 = document.getElementById('username1').value;
-	var input2 = document.getElementById('Fname').value;
-	var input3 = document.getElementById('Lname').value;
-	var input4 = document.getElementById('pw').value;
-	var input5 = document.getElementById('maildetail').value;
-	alert("Register Complete");
-	//localStorage.setItem('username', username1);
-	//localStorage.setItem('FName', Fname);
-	//localStorage.setItem('LName', Lname);
-	//localStorage.setItem('pw', pw);
-	//localStorage.setItem('email', maildetail)
+	window.location="connectionparent.html";
 }
-function unitestpages()
+function play()
 {
-	if(window.location("register.html")==null)
-		return alert("register page error")+false;
-	if(window.location("connection.html")==null)
-		return alert("connection page error")+false;
-	if(window.location("userguide.html")==null)
-		return alert("userguide page error")+false;
-	if(window.location("FrontPageExample.html")==null)
-		return alert("FrontPageExample page error")+false;
-}
-function unitestuserdetails(username1,Fname,Lname,pw,maildetail)
-{
-	if (username1 == null)
-		return alert("username wrong") + false;
-	if (Fname == null)
-		return alert("First Name wrong") + false;
-	if (Lname == null)
-		return alert("Last Name wrong") + false;
-	if (pw == null)
-		return alert("password Wrong") + false;
-	if (maildetail == null)
-		return alert("mail wrong") + false;
+    window.location=("https://kalvasefer.000webhostapp.com/Levels.html");
 }
 function LionExam()
 {
-	window.location="LionExam.html"
+	window.location="LionExam.php";
+}
+function BaloonExam()
+{
+	window.location="FiveBaloonsExam.php";
 }
 function CheckAnswer(Question,Check)
 {
-	if (Question == 1) alert("yes")
-	else alert("nono")
+	if (Question == 1) alert("yes");
+	else alert("nono");
+}
+function parent_login()
+{
+    window.location="parentlogin.php";
+}
+function moveexam()
+{
+ location.href ="https://kalvasefer.000webhostapp.com/Levels.html";
+}
+function GetGrades()
+{
+    window.location="checktable.php";
+}
+function report(){
+     window.location="https://kalvasefer.000webhostapp.com/report.php";
+}
+function report1()
+{
+     window.location="https://kalvasefer.000webhostapp.com/reportuser.php";
+}
+function ParentHomePage(){
+      window.location="ParentHomePage.html";
+}
+function connectionparent(){
+     window.location="connectionparent.html";
+}
+function userguideparent(){
+     window.location="userguideparent.html";
+}
+function reportparent(){
+     window.location="reportparent.php";
+}
+function takegrade(){
+     window.location="takegrade2.html";
+}
+function CheckBaloonExam1()
+{
+    window.location="checktable5.php";
+}   
+function CheckPinkyExam2()
+{
+    window.location="checktable6.php";
+}
+function CheckBaloonExam()
+{
+    window.location="checktable2.php";
+}   
+function Checklionexam1()
+{
+    window.location="checktable4.php";
+}
+function adminlogin(){
+     window.location="adminlogin.php";
+}
+function adminupdateadult(){
+    window.location="adminupdateadult.php";
+}
+function adminupdatechild(){
+    window.location="adminupdatechild.php";
+}
+function report2(){
+    window.location="report2.php";
+}
+function userguideadmin(){
+    window.location="userguideadmin.html";
+}
+function adminhomepage(){
+    window.location="adminhomepage.php";
 }
